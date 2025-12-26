e4f2a5b0f5c112d819b5e2b2a9b66953b9dafb73
bbangme1 <bbangme10@gmail.com>
2025-12-26 16:24:14 +0900

    Add files via upload

---
+++ AccessControl/src/ex02/AccessContril2.java
@@ -0,0 +1,12 @@
+package ex02;
+
+import ex01.AccessControlEx01;
+
+public class AccessContril2 extends AccessControlEx01 {
+
+    int myMoney;
+
+    public void text(){
+        myMoney = shareMoney;
+    }
+}
---
+++ AccessControl/src/ex02/AccessControlUse.java
@@ -0,0 +1,18 @@
+package ex02;
+
+import ex01.AccessControlEx01;
+
+public class AccessControlUse {
+    public static void main(String[] args) {
+        AccessControlEx01 ac = new AccessControlEx01();
+        System.out.println(ac.getNum());
+
+        // ac.name = "홍길동";
+        ac.setName("홍길동");
+//        ac.setNum(1000);
+//        ac.shareMoney -= 10000;
+
+        System.out.println(ac.getNum());
+
+    }
+}
